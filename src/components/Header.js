5f28818ceb913ed20d535f40e2a964498ec37ce3
Enrique Rodriguez Vela <enriquemondejar15@gmail.com>
2024-08-13 14:10:28 +0200

    chore: Improve header design and search functionality
    The code changes in the Header component include updating the logo size, adding a margin to the heading, and improving the search input styling. The placeholder text for the search input has also been updated.

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,18 @@ const Header = () => {
     <header className="sticky top-0 left-0 w-full bg-white p-6 flex justify-between items-center z-50">
       <div className="flex items-center">
         <Image className="w-10 h-10" src={Logo} alt="Logo" width={40} height={40} />
-        <h1 className='rounded'><a href='/'>MascotasAi</a></h1>
+        <h1 className='ml-2 text-xl font-bold'><a href='/'>MascotasAi</a></h1>
       </div>
-      <div className="flex-1 mx-6">
+      <div className="absolute left-1/2 transform -translate-x-1/2 w-full max-w-2xl px-6">
         <input
-          className="w-full max-w-xl p-2 border rounded bg-white text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-600"
+          className="w-full p-2 border rounded bg-white text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-600"
           type="text"
-          placeholder="Search..."
+          placeholder="Busca ideas... (todavia no esta implementado)"
           aria-label="Search"
         />
       </div>
       <div className="flex items-center">
-        {/* <Image className="w-10 h-10 rounded-full" src="/images/avatar.svg" alt="User Avatar" width={40} height={40} /> */}
+        {/* Add user avatar or other elements here */}
       </div>
     </header>
   );
